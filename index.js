160c2454dc8fb1f20daa633c1a19b441ef0e1a0a
root <yluo3421@gmail.com>
2022-04-25 17:37:13 -0400

    commit tree 3092a34d35cb4396e0f999fc7e59b65ecf134c03

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // code your solution here
+function saturdayFun(activity = 'roller-skate') {
+    let answer = `This Saturday, I want to ${activity}!`;
+    return answer;
+}
+
+function mondayWork(activity = 'go to the office') {
+    let answer = `This Monday, I will ${activity}.`;
+    return answer;
+
+}
+
+function wrapAdjective(visualFlair = '*') {
+    let emphatic = '';
+    if (visualFlair === '*') {
+           emphatic = 'a hard worker';
+    }
+    else if (visualFlair === '||') {
+        emphatic = 'a dedicated programmer';   
+    }
+    const inner = function (emphatic) {
+        return `You are ${visualFlair}${emphatic}${visualFlair}!`;
+    }
+    return inner;
+
+}
